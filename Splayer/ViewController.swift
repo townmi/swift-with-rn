3f5f264becd07c7d25884854a94fdc64af49fd2e
Harry Tang <harry@harrys-macbook-pro.local>
2019-02-12 14:38:05 +0800

    Initial Commit

---
+++ Splayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Splayer
+//
+//  Created by Harry Tang on 2019/2/12.
+//  Copyright © 2019 Harry Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
